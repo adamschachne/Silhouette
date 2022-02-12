18aa9e0dffd150a8629b7ac1fa4f934723c3e31d
Adam Schachne <adamschachne@live.com>
2022-02-12 14:39:17 -0800

    Smooth rotations and simplify the logic

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -10,31 +10,17 @@ public class Wall : MonoBehaviour
     private const string POLY_TAG = "Poly";
     private const string UNTAGGED_TAG = "Untagged";
     public Vector3 wallScale;
-    public Vector3 otherWallScale;
-    private float OFFSET = 0.001f;
+    private const float OFFSET = 0.001f;
 
     // given a clone and box, modify the clone's transform so that it forms a shadow on this wall
     private void SetCloneShadowOnWall(GameObject clone, GameObject poly)
     {
-        //Vector3 wallUp = this.transform.up;
         Vector3 wallPosition = this.transform.position;
-
         Vector3 polyPosition = poly.transform.position;
-        Vector3 scale = poly.transform.localScale;
-        float x = (polyPosition.x * wallScale.x) + wallPosition.x - Mathf.Sign(wallPosition.x);
-        float z = (polyPosition.z * wallScale.z) + wallPosition.z - Mathf.Sign(wallPosition.z);
+        float x = (polyPosition.x * wallScale.x) + wallPosition.x;
+        float z = (polyPosition.z * wallScale.z) + wallPosition.z;
         clone.transform.position = new Vector3(x, clone.transform.position.y, z);
         clone.transform.rotation = poly.transform.rotation;
-
-        // "flatten" the clone on the wall
-        if (clone.transform.eulerAngles.y % 180 == 0)
-        {
-            clone.transform.localScale = new Vector3(wallScale.x * scale.x, wallScale.y * scale.y, wallScale.z * scale.z);
-        }
-        else
-        {
-            clone.transform.localScale = new Vector3(otherWallScale.x * scale.x, otherWallScale.y * scale.y, otherWallScale.z * scale.z);
-        }
     }
 
     // Start is called before the first frame update
@@ -48,9 +34,14 @@ public class Wall : MonoBehaviour
             // Make a clone of the box
             GameObject poly = polys[i];
             GameObject clone = Instantiate(poly);
+            GameObject shadow = new GameObject();
+            shadow.name = $"{poly.name} shadow on {this.name}";
+            clone.transform.SetParent(shadow.transform);
+            shadow.transform.localScale = wallScale;
+            shadow.transform.position = new Vector3(wallPosition.x - (1 - wallScale.x) * OFFSET, 0, wallPosition.z - (1 - wallScale.z) * OFFSET);
+
             clone.tag = UNTAGGED_TAG;
             clones[i] = clone;
-            Vector3 scale = poly.transform.localScale;
          
             // Set the transform and box rotation on the wall
             SetCloneShadowOnWall(clone, poly);
