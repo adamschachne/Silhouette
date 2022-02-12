6db58fe690493c39554f2e15652d898bba76fd67
Yu Zheng <zhen405@usc.edu>
2022-02-12 13:09:48 -0800

    Sort of fixed the polycubes' rotation

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -9,6 +9,7 @@ public class Wall : MonoBehaviour
     private GameObject[] clones;
     private const string POLY_TAG = "Poly";
     public Vector3 wallScale;
+    public Vector3 otherWallScale;
     private float OFFSET = 0.001f;
 
     // given a clone and box, modify the clone's transform so that it forms a shadow on this wall
@@ -18,10 +19,21 @@ public class Wall : MonoBehaviour
         Vector3 wallPosition = this.transform.position;
 
         Vector3 polyPosition = poly.transform.position;
-        float x = (polyPosition.x * wallScale.x) + wallPosition.x - Mathf.Sign(wallPosition.x) * OFFSET;
-        float z = (polyPosition.z * wallScale.z) + wallPosition.z - Mathf.Sign(wallPosition.z) * OFFSET;
+        Vector3 scale = poly.transform.localScale;
+        float x = (polyPosition.x * wallScale.x) + wallPosition.x - Mathf.Sign(wallPosition.x);
+        float z = (polyPosition.z * wallScale.z) + wallPosition.z - Mathf.Sign(wallPosition.z);
         clone.transform.position = new Vector3(x, clone.transform.position.y, z);
         clone.transform.rotation = poly.transform.rotation;
+
+        // "flatten" the clone on the wall
+        if (clone.transform.eulerAngles.y % 180 == 0)
+        {
+            clone.transform.localScale = new Vector3(wallScale.x * scale.x, wallScale.y * scale.y, wallScale.z * scale.z);
+        }
+        else
+        {
+            clone.transform.localScale = new Vector3(otherWallScale.x * scale.x, otherWallScale.y * scale.y, otherWallScale.z * scale.z);
+        }
     }
 
     // Start is called before the first frame update
@@ -37,10 +49,7 @@ public class Wall : MonoBehaviour
             GameObject clone = Instantiate(poly);
             clones[i] = clone;
             Vector3 scale = poly.transform.localScale;
-
-            // "flatten" the clone on the wall
-            clone.transform.localScale = new Vector3(scale.x * wallScale.x, scale.y * wallScale.y, scale.z * wallScale.z);
-
+         
             // Set the transform and box rotation on the wall
             SetCloneShadowOnWall(clone, poly);
         }
