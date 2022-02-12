97cc8e5e3d17eb31a3847cbb0f8ecb4f5c7f11ce
Adam Schachne <adamschachne@live.com>
2022-02-12 14:49:53 -0800

    Change level 1 and reduce outline width

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,6 +6,6 @@ public static class Constants
 {
     public static Dictionary<string, int> LevelMap { get; set; } = new Dictionary<string, int>
     {
-        {"Yuni-Scene", 1 }
+        {"Poly-Cubes", 1 }
     };
 }
