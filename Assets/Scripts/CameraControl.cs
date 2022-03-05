28fb449a0fceada14f9e7996ef3bc284abb80988
kushalaa <kushi.hande@gmail.com>
2022-03-04 23:31:07 -0800

    Rotate camera based on distance

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -18,6 +18,8 @@ public class CameraControl : MonoBehaviour
     public float angleMax = 43.0f;
 
     private float sensitivity = 0.5f; 
+
+    private Vector3 dragOrigin;
      private Vector3 initialVector = Vector3.forward;
     private void DeselectAllPolys()
     {
@@ -49,6 +51,27 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // Pressing mouse 1 AND not pressing the buttons
+        if (Input.GetMouseButtonDown(0) && UnityEngine.EventSystems.EventSystem.current != null &&
+            !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit[] hits;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hits = Physics.RaycastAll(ray, MAX_RAYCAST_DIST).Where(hit => hit.transform.gameObject.CompareTag(BOX_TAG)).ToArray();
+            if (hits.Length > 0) // Hit a "Box" -> Select it
+            {
+                System.Array.Sort(hits, (hit1, hit2) => hit1.distance < hit2.distance ? -1 : 1);
+                SelectPoly(hits[0].transform.parent.gameObject);
+            }
+            else // Clicked on nothing -> Deselect selected box
+            {
+                DeselectAllPolys();
+            }
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
         float rotateDegrees = 0f;
         bool keyPressed = false;
         // Pressing A or LeftArrow -> Rotate the camera left
@@ -72,18 +95,19 @@ public class CameraControl : MonoBehaviour
        // Left click and drag to rotate the camera 
        // && UnityEngine.EventSystems.EventSystem.current != null && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()
         else if (Input.GetMouseButton(0) ) {
-        var rotateIn = 0.0f;
-       if (Application.platform == RuntimePlatform.WebGLPlayer) {
-            
-            Debug.Log("Inside WEBGL");    
-            sensitivity = 1.0f / 75;
-        }
-            
-        else {
-            
-            sensitivity = 0.5f;
-            Debug.Log("Inside NON WEBGL");
-        }
+
+            // if (Application.platform == RuntimePlatform.WebGLPlayer) {
+                    
+            //         // Debug.Log("Inside WEBGL");    
+            //         sensitivity = 1.0f / 75;
+            // } else {
+                    
+            //         sensitivity = 0.5f;
+            //         // Debug.Log("Inside NON WEBGL");
+            // }
+            // rotateDegrees = ROTATE_SPEED_DRAG * Input.GetAxis("Mouse X") * sensitivity;
+            // keyPressed = true;
+            //------------------------------------------------------------------------------
             // var x = Input.GetAxis("Mouse X");
             // var y = Input.GetAxis("Mouse Y");
             // if (Application.platform == RuntimePlatform.WebGLPlayer) {
@@ -92,11 +116,11 @@ public class CameraControl : MonoBehaviour
             // }
             // x *= SENSITIVITY; // custom sensitivity value...
             // y *= SENSITIVITY; 
-            rotateDegrees = ROTATE_SPEED_DRAG * Input.GetAxis("Mouse X") * sensitivity;
-            keyPressed = true;
             // this.transform.RotateAround(board.transform.position, -Vector3.up, ROTATE_SPEED * Time.deltaTime);
-
-
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            rotateDegrees = ROTATE_SPEED_DRAG * pos.x *0.1f;
+            Debug.Log(rotateDegrees); 
+            keyPressed = true;
         }
 
         // Rotate the camera if key is pressed or long press and drag left click
@@ -104,34 +128,14 @@ public class CameraControl : MonoBehaviour
             //this.transform.RotateAround(board.transform.position, -Vector3.up, ROTATE_SPEED * Time.deltaTime);
             if(board.transform != null) {
 
-                Vector3 currentVector = transform.position- board.transform.position;
+                Vector3 currentVector = transform.position - board.transform.position;
                 currentVector.y = 0;
                 float angleBetween = Vector3.Angle(initialVector, currentVector) * (Vector3.Cross(initialVector, currentVector).y > 0 ? 1 : -1);            
                 float newAngle = Mathf.Clamp(angleBetween + rotateDegrees, -angleMax, angleMax);
                 rotateDegrees = newAngle - angleBetween;
-            
                 this.transform.RotateAround(board.transform.position, Vector3.up, rotateDegrees);
                 arrowKeys.transform.RotateAround(arrowKeys.transform.position, Vector3.forward, rotateDegrees);
             }
-        }
-
-        // Pressing mouse 1 AND not pressing the buttons
-        if (Input.GetMouseButtonDown(0) && UnityEngine.EventSystems.EventSystem.current != null &&
-            !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
-        {
-            RaycastHit[] hits;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            hits = Physics.RaycastAll(ray, MAX_RAYCAST_DIST).Where(hit => hit.transform.gameObject.CompareTag(BOX_TAG)).ToArray();
-            if (hits.Length > 0) // Hit a "Box" -> Select it
-            {
-                System.Array.Sort(hits, (hit1, hit2) => hit1.distance < hit2.distance ? -1 : 1);
-                SelectPoly(hits[0].transform.parent.gameObject);
-            }
-            else // Clicked on nothing -> Deselect selected box
-            {
-                DeselectAllPolys();
-            }
-            return;
         }
 
     }
