d1def36ab37f2253cbe24dcc6ceb38f0321f190e
Olaseeni Olayinka <olaseeniolay@gmail.com>
2022-04-12 15:31:32 -0700

    added the menu

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -17,20 +17,25 @@ public class SettingsMenu : MonoBehaviour
 
     public void LevelOne() {
         // Application.LoadLevel("Level 1");
-        SceneManager.LoadScene("Level 2");
+        SceneManager.LoadScene(3, LoadSceneMode.Additive);
     }
 
     public void LevelTwo() {
         // Application.LoadLevel("Level 2");
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4, LoadSceneMode.Additive);
     }
 
     public void LevelThree() {
         // Application.LoadLevel("Level 3");
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(5, LoadSceneMode.Additive);
     }
 
-    public void LevelFour() {}
-    public void LevelFive() {}
-    public void Back() {}
+    public void LevelFour() {
+        SceneManager.LoadScene(6, LoadSceneMode.Additive);
+    }
+    // public void LevelFive() {}
+
+    public void tutorial() {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
 }
