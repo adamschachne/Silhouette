241d8b01e06630106c39c961ff444aaa725ac594
Rakshith Gowda VR <rrrakshi10@gmail.com>
2022-04-14 14:22:25 -0700

    Timer functionality with seconds granularity added

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -8,18 +8,11 @@ public class Timer : MonoBehaviour
 {
 
     public TextMeshProUGUI timerText;
-    private static float startTime = 0.0f;
     private bool pause = false;
-    private float pauseTimeStart;
-    private float pausedTime;
-    private static float t = 0.0f;
+    private static float timeToDisplay = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        // if(startTime < 0.001f) {
-        //     startTime = Time.time;
-        // }
-        // pausedTime = 0.0f;
     }
 
     // Update is called once per frame
@@ -28,33 +21,24 @@ public class Timer : MonoBehaviour
         if(pause) {
             return;
         }
-        // float t = Time.deltaTime;
-        // float t = Time.time - startTime - pausedTime;
-        t += Time.deltaTime;
-        string minutes = ((int) t / 60 ).ToString();
-        string seconds = (t%60).ToString("f2");
-
-        timerText.text = minutes + ":" + seconds;
+        timeToDisplay += Time.deltaTime;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);  
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     public void Pause()
     {
         pause = true;
-        // pauseTimeStart = Time.time;
     }
 
     public void Play()
     {
         pause = false;
-        // pausedTime += Time.time - pauseTimeStart;
     }
 
     public void resetTimer()
     {
-        // Debug.Log("start" + startTime);
-        // startTime = 0.0f;
-        // pausedTime = 0.0f;
-        t = 0.0f;
-        // Debug.Log(" start after pause "+startTime);
+        timeToDisplay = 0.0f;
     }
 }
