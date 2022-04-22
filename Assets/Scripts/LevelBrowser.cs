37ebcdc46b1455ba301547be0f96a3fa596f6495
Olaseeni Olayinka <olaseeniolay@gmail.com>
2022-04-21 18:55:10 -0700

    moved the position of the menu and added instructions to the menu

--- Assets/Scripts/LevelBrowser.cs
+++ Assets/Scripts/LevelBrowser.cs
@@ -13,7 +13,7 @@ public class LevelBrowser : MonoBehaviour
     private void OnEnable() {
         levelManager = GameObject.Find("GameManager").GetComponent<LevelManager>();
         
-        for(int i = 0; i < levelManager.levels.Length; i++) {
+        for(int i = 3; i < levelManager.levels.Length; i++) {
             
             int levelNumber = i;
             GameObject newButton = Instantiate(buttonPrefab, buttonParent.transform);
