c480daa8c3cc02aff8836bfe8d24a7f71befbbe5
kushalaa <kushi.hande@gmail.com>
2022-04-22 12:47:57 -0700

    Pop up integrated with tutorial script

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     // Start is called before the first frame update
+    public PopupMessage dialogBox;
     void Start()
     {
         CameraControl.ClickEvent += Test;
+        dialogBox = GameObject.Find("SolutionManager").GetComponent<PopupMessage>();
+        Debug.Log("Inside start");
+        if(dialogBox == null) {
+           Debug.Log("Inside null"); 
+        }
+        dialogBox.popUpWithMsg("Click on the box to select it - from script");
     }
 
     private void Test() {
         Debug.Log("CLICK EVENT ***************");
+        dialogBox.closePopUp();
     }
 
     // Update is called once per frame
