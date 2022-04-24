bffb33767eaa9c255584b2be9bd22ec0d001140d
Olaseeni Olayinka <46079073+OlaseeniOlayinka@users.noreply.github.com>
2022-04-23 18:13:38 -0700

    merge local and remote

---
+++ Assets/Scripts/LevelBrowser.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelBrowser : MonoBehaviour
+{
+    public GameObject buttonPrefab;
+    public GameObject buttonParent;
+
+    private LevelManager levelManager;
+    
+    private void OnEnable() {
+        levelManager = GameObject.Find("GameManager").GetComponent<LevelManager>();
+        
+        for(int i = 0; i < levelManager.levels.Length; i++) {
+            
+            int levelNumber = i;
+            GameObject newButton = Instantiate(buttonPrefab, buttonParent.transform);
+    
+            if (i <= 2) {
+                
+                string textForBtn = "Tutorial " + (i+1).ToString();
+                
+                newButton.GetComponent<LevelButton>().levelText.text = textForBtn;
+                Debug.Log(newButton.GetComponent<LevelButton>().levelText.text + " " + i);
+            }
+            else {
+                string textForBtn = "Level " + (i-2).ToString();
+
+                newButton.GetComponent<LevelButton>().levelText.text = textForBtn;
+                Debug.Log(newButton.GetComponent<LevelButton>().levelText.text + " " + i);
+            }
+            newButton.GetComponent<Button>().onClick.AddListener(() => {
+                Debug.Log("Current level " + levelNumber);
+                PlayerData.CurrentLevel = levelNumber;
+                levelManager.LoadLevel();
+
+            } );//SelectedLevel(i)
+
+            
+        }
+    }
+
+    private void SelectedLevel(int currLevel) {
+        Debug.Log("Current level " + currLevel);
+        PlayerData.CurrentLevel = currLevel;
+        
+        levelManager.LoadLevel();
+    }
+}
